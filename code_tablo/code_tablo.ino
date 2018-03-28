3f68d0a4e8e2f56b0adf0506299a31e5a19d1b7b
sergei <sergei@it108.org>
2018-03-28 16:28:10 +0300

    func clock

--- code_tablo/code_tablo.ino
+++ code_tablo/code_tablo.ino
@@ -1,14 +1,34 @@
 const char D = 2;   //serial        *output
-const char OE = 3;  //output enable *output 
+const char OE = 3;  //output enable *output
 const char STR = 4; // strobe       *output
 const char CP = 5;  //clock         *output
 
-bool stateD =0;
-bool stateOE=0;
-bool stateSTR=0;
-bool statePC=0;
+bool stateD = 0;
+bool stateOE = 0;
+bool stateSTR = 0;
+bool stateCP = 0;
 
 long long a;
+
+void clockSend() {
+  if (stateCP) {
+    digitalWrite(CP, LOW);
+    stateCP = 0;
+  } else {
+    digitalWrite(CP, HIGH);
+    stateCP = 1;
+  }
+}
+
+void strobSend() {
+  if (stateSTR) {
+    digitalWrite(STR, LOW);
+    stateSTR = 0;
+  } else {
+    digitalWrite(STR, HIGH);
+    stateSTR = 1;
+  }
+}
 
 void setup() {
   pinMode(D, OUTPUT);
@@ -18,6 +38,6 @@ void setup() {
 }
 
 void loop() {
-  
+
 
 }
