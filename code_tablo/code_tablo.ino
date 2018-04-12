998642310739b9c3983e5c3fd55134286aef387d
sergei <sergei@it108.org>
2018-04-12 20:44:07 +0300

    working code_funcs

--- code_tablo/code_tablo.ino
+++ code_tablo/code_tablo.ino
@@ -1,71 +1,98 @@
-const char D = 2;   //serial        *output
+const char D = 6;   //serial        *output
 const char OE = 3;  //output enable *output
 const char STR = 4; // strobe       *output
-const char CP = 5;  //clock         *output
-
+const char CP = 7;  //clock         *output
+const char LED = 13;
 bool stateD = 0;
 bool stateOE = 1;
 bool stateSTR = 1;
 bool stateCP = 0;
-
+bool stateLED = 0;
 long long a;
 
 
-void dataSend() {
-  if (stateD) {
-    digitalWrite(D, LOW);
-    stateD = 0;
+void ledSend() {
+  if (stateLED) {
+    digitalWrite(LED, LOW);
+    stateLED = 0;
   } else {
-    digitalWrite(D, HIGH);
-    stateD = 1;
+    digitalWrite(LED, HIGH);
+    stateLED = 1;
   }
   delay(1);
 }
+void dataDown() {
+  digitalWrite(D, LOW);
+  stateD = 0;
+  delay(1);
+}
 
-void clockSend() {
-  if (stateCP) {
-    digitalWrite(CP, LOW);
-    stateCP = 0;
-  } else {
-    digitalWrite(CP, HIGH);
-    stateCP = 1;
-  }
+void dataUp() {
+  digitalWrite(D, HIGH);
+  stateD = 1;
+  delay(1);
+}
+
+
+void clockDown() {
+  digitalWrite(CP, LOW);
+  stateCP = 0;
+  ledSend();
   delay(100);
 }
 
-void strobSend() {
-  if (stateSTR) {
-    digitalWrite(STR, LOW);
-    stateSTR = 0;
-  } else {
-    digitalWrite(STR, HIGH);
-    stateSTR = 1;
-  }
+void clockUp() {
+  digitalWrite(CP, HIGH);
+  stateCP = 1;
+  delay(100);
 }
 
+void numSend1() {
+  dataDown();
+  clockUp(); clockDown();
+  clockUp(); clockDown();
+  clockUp(); clockDown();
+  clockUp(); clockDown();
+  clockUp(); clockDown();
+  clockUp(); clockDown();
+  clockUp(); clockDown();
+  clockUp(); clockDown();
+  dataUp();
+  clockUp(); clockDown();
+  clockUp(); clockDown();
+  clockUp(); clockDown();
+  clockUp(); clockDown();
+  dataDown();
+  clockUp(); clockDown();
+  dataUp();
+  clockUp(); clockDown();
+  dataDown();
+  clockUp(); clockDown();
+  dataUp();
+  clockUp(); clockDown();
+
+}
 void setup() {
+  Serial.begin(9600);
+
+
   pinMode(D, OUTPUT);
   pinMode(OE, OUTPUT);
   pinMode(STR, OUTPUT);
   pinMode(CP, OUTPUT);
+  pinMode(LED, OUTPUT);
 
-
-
+  dataDown();
+  for (char i = 0; i < 32; i++) {
+    clockUp();
+    clockDown();
+  }
+  numSend1();
+  numSend1();
 }
 
 void loop() {
-  dataSend();    //up
-  clockSend();   //up
-  
-  clockSend();   //down
-  
- // dataSend();    //down
-  clockSend();   //up
-  
-  clockSend();   // down
 
-
- 
 
 
 }
