d42faab248db8ce81c93d1636c88b49e416be410
Unknown <sergei-filippov2001@yandex.ru>
2018-05-05 12:44:17 +0300

    everyth is written but not tested

--- code/clock1/clock1.ino
+++ code/clock1/clock1.ino
@@ -1,11 +1,6 @@
-#include <Time.h>
-#include <TimeLib.h>
-
 #include <DS1307RTC.h>
-
-
-
-#include <Wire.h>                           // Подключаем библиотеку Wire                           
+#include <TimeLib.h>
+#include <Wire.h>                                                
                   
           
  
@@ -13,31 +8,15 @@ void setup() {
   Serial.begin(9600);                       // Устанавливаем скорость передачи данных 
   //while (!Serial) ;                         // Ожидаем подключение последовательного порта. Нужно только для Leonardo
   delay(200);                               // Ждем 200 мкс
-  Serial.println("DS3231RTC Read Test");    // Выводим данные на последовательный порт
-  Serial.println("-------------------");    // Выводим данные на последовательный порт
+ /* Serial.println("DS3231RTC Read Test");    // Выводим данные на последовательный порт
+  Serial.println("-------------------");    // Выводим данные на последовательный порт*/
 }
  
 void loop() {
-  Serial.println ("g");
  tmElements_t tm;                          
- Serial.println ("gg");
   if (RTC.read(tm)) {
-    Serial.println ("ggg");
-    Serial.print("Ok, Time = ");
-    print2digits(tm.Hour);
-    Serial.write(':');
-    print2digits(tm.Minute);
-    Serial.write(':');
-    print2digits(tm.Second);
-    Serial.print(", Date (D/M/Y) = ");
-    Serial.print(tm.Day);
-    Serial.write('/');
-    Serial.print(tm.Month);
-    Serial.write('/');
-    Serial.print(tmYearToCalendar(tm.Year));
-    Serial.println();
+        Serial.println(tm.Day);
   } else {
-    Serial.println ("gggg");
     if (RTC.chipPresent()) {
       Serial.println("The DS3231 is stopped.  Please run the SetTime");
       Serial.println("example to initialize the time and begin running.");
@@ -48,7 +27,7 @@ void loop() {
     }
     delay(9000);
   }
-  delay(1000);
+  delay(2000);
 }
  
 void print2digits(int number) {
---
+++ code/code_tablo_clock/code_tablo_clock.ino
@@ -0,0 +1,370 @@
+#include <DS1307RTC.h>
+#include <TimeLib.h>
+#include <Wire.h>
+
+
+
+
+const char D = 6;   //serial        *output
+const char OE = 3;  //output enable *output
+const char STR = 4; // strobe       *output
+const char CP = 7;  //clock         *output
+const char LED = 13;
+bool stateD = 0;
+bool stateOE = 1;
+bool stateSTR = 1;
+bool stateCP = 0;
+bool stateLED = 0;
+long long a;
+char Day;
+int numberD;
+
+
+void ledSend() {
+  if (stateLED) {
+    digitalWrite(LED, LOW);
+    stateLED = 0;
+  } else {
+    digitalWrite(LED, HIGH);
+    stateLED = 1;
+  }
+  delay(1);
+}
+void dataDown() {
+  digitalWrite(D, LOW);
+  stateD = 0;
+  delay(1);
+}
+
+void dataUp() {
+  digitalWrite(D, HIGH);
+  stateD = 1;
+  delay(1);
+}
+void cUD() {
+  clockUp();
+  clockDown();
+}
+
+void clockDown() {
+  digitalWrite(CP, LOW);
+  stateCP = 0;
+  ledSend();
+  delay(100);
+}
+
+void clockUp() {
+  digitalWrite(CP, HIGH);
+  stateCP = 1;
+  delay(100);
+}
+void anyNum(int numberD) {
+  switch (numberD) {
+    case 0: numSend0(); break;
+    case 1: numSend1(); break;
+    case 2: numSend2(); break;
+    case 3: numSend3(); break;
+    case 4: numSend4(); break;
+    case 5: numSend5(); break;
+    case 6: numSend6(); break;
+    case 7: numSend7(); break;
+    case 8: numSend8(); break;
+    case 9: numSend9(); break;
+  }
+}
+
+void numSend0() {
+  //--microchip 2 --//
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  //--microchip 1 --//
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+}
+void numSend9() {
+  //--microchip 2 --//
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  //--microchip 1 --//
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+}
+
+void numSend8() {
+  //--microchip 2 --//
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  //--microchip 1 --//
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+}
+
+void numSend7() {
+  //--microchip 2 --//
+  dataDown();
+  cUD();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  dataDown();
+  cUD();
+  cUD();
+  cUD();
+  //--microchip 1 --//
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+}
+void numSend6() {
+  //--microchip 2 --//
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  //--microchip 1 --//
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+}
+
+void numSend5() {
+  //--microchip 2 --//
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  //--microchip 1 --//
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+}
+void numSend4() {
+  //--microchip 2 --//
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  dataDown();
+  cUD();
+  cUD();
+  dataUp();
+  cUD();
+  dataDown();
+  cUD();
+  cUD();
+  //--microchip 1 --//
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+}
+
+void numSend3() {
+  //--microchip 2 --//
+  dataUp();
+  cUD();
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  //--microchip 1 --//
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+}
+void numSend2() {
+  dataUp();
+  cUD();
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  //--microchip 1 --//
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  dataDown();
+  cUD();
+}
+
+void numSend1() {
+  dataDown();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  //--microchip 1 --//
+  dataUp();
+  cUD();
+  cUD();
+  cUD();
+  cUD();
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+  dataDown();
+  cUD();
+  dataUp();
+  cUD();
+}
+
+
+void print2digits(int number) {
+  if (number >= 0 && number < 10) {
+    Serial.write('0');
+  }
+  Serial.print(number);
+}
+
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(D, OUTPUT);
+  pinMode(OE, OUTPUT);
+  pinMode(STR, OUTPUT);
+  pinMode(CP, OUTPUT);
+  pinMode(LED, OUTPUT);
+
+  dataDown();
+  for (char i = 0; i < 32; i++) {
+    clockUp();
+    clockDown();
+  }
+}
+
+void loop() {
+  tmElements_t tm;
+  if (RTC.read(tm)) {
+    Serial.println(tm.Day);
+    Day = tm.Day;
+    anyNum(Day / 10);
+    anyNum(Day % 10);
+  }
+  delay(5000);
+}
